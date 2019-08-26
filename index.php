603adff036f9636a0fdfb668eb019de1cdb140f8
Daniel Jungwirth <s3539595@student.rmit.edu.au>
2019-08-26 13:43:19 +1000

    added relevant data from ass2 spec to assignment

--- index.php
+++ index.php
@@ -8,10 +8,10 @@
         <link id='wireframecss' type="text/css" rel="stylesheet" href="../wireframe.css" disabled>
         <link id='stylecss' type="text/css" rel="stylesheet" href="css/style.css">
         <script src='../wireframe.js'></script>
-        
-        
-        
-        
+
+
+
+
     </head>
 
     <body>
@@ -42,185 +42,223 @@
                     <br>
                     <br>
                     <h1>About Us </h1>
-                    
+
                     <P>
+
+                        The client wants to target not only their loyal customer from before, but also local cinema goers who would normally drive to modern cinemas in nearby larger towns.
                         <br>
-                        cat
+                        This area should tell (ie with words) and show (ie with images) potential customers these things:
+                        The cinema has reopened after extensive improvements and renovations.
+                        There are new seats: standard seats and reclinable first class seats
+                        The projection and sound systems are upgraded with 3D Dolby Vision projection and Dolby Atmos sound. See https://www.dolby.com/us/en/cinema for more information.
                         <br>
-                        dog
-                        <br>
-                        horse
-                        <br>
-                        pig
-                        <br>
-                        chimp
-                        <br>
-                        greg
-                        <br>
-                        paul
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
+                        The client wants to show pictures of their new seats: standard seating and first class seating. Images for the seats can be found here:
+                        Standard: http://www.profurn.com.au/portfolio-item/538/
+                        First Class: http://www.profurn.com.au/portfolio-item/verona-single-zero-wall/
+
+
+
+
                     </P>
                 </article>
 
 
                 <article  id="linktoPrices">
-                      <br>
+                    <br>
                     <br>
                     <h1>Prices </h1>
                     <P>
-                        <br>
-                        cat
-                        <br>
-                        dog
-                        <br>
-                        horse
-                        <br>
-                        pig
-                        <br>
-                        chimp
-                        <br>
-                        greg
-                        <br>
-                        paul
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
+                        <table>
+                            <tr>
+                                <th>Seat Type</th>
+                                <th>Seat Code</th>
+                                <th>All day Monday and Wednesday AND 12pm on Weekdays </th>
+                                <th>All Other Times</th>
+                            </tr>
+                            <tr>
+                                <td>Standard Adult</td>
+                                <td>STA</td> 
+                                <td>14.00</td>
+                                <td>19.80</td>
+                            </tr>
+                            <tr>
+                                <td>Standard Concession</td>
+                                <td>STP</td> 
+                                <td>12.50</td>
+                                <td>17.50</td>
+                            </tr>
+                            <tr>
+                                <td>Standard Child</td>
+                                <td>STC</td> 
+                                <td>11.00</td>
+                                <td>15.30</td>
+                            </tr>
+                            <tr>
+                                <td>First Class Adult</td>
+                                <td>FCA</td> 
+                                <td>24.00</td>
+                                <td>30.00</td>
+                            </tr>
+                            <tr>
+                                <td>First Class Concession</td>
+                                <td>FCP</td> 
+                                <td>22.50</td>
+                                <td>27.00</td>
+                            </tr>
+                            <tr>
+                                <td>First Class Child </td>
+                                <td>FCC</td> 
+                                <td>21.00</td>
+                                <td>24.00</td>
+                            </tr>
+                        </table>
                     </P>
                 </article>
 
 
                 <article  id="linktoNowShowing">
-                      <br>
+                    <br>
                     <br>
                     <h1>Now Showing</h1>
                     <P>
-                        <br>
-                        cat
-                        <br>
-                        dog
-                        <br>
-                        horse
-                        <br>
-                        pig
-                        <br>
-                        chimp
-                        <br>
-                        greg
-                        <br>
-                        paul
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
+
+
+                        <div class="nowShowingCard">
+                            <img src="img_avatar.png" alt="Avatar">
+                            <div class="container">
+                                <h4>Avengers: Endgame</h4> 
+                                <h6> SHOWING:</h6>
+                                <p>
+                                    Wed - Fri: 9pm (T21)<br>
+                                    Sat - Sun: 6pm (T18)<br>
+                                </p>
+                            </div>
+                        </div>
+
+                        <div class="nowShowingCard">
+                            <img src="img_avatar.png" alt="Avatar" style="width:100%">
+                            <div class="container">
+                                <h4>Top End Wedding</h4> 
+                                <h6> SHOWING:</h6>
+                                <p>
+                                    Mon - Tues: 6pm (T18)<br>
+                                    Sat - Sun: 3pm (T15)<br>
+                                </p>
+                            </div>
+                        </div>  
+
+
+                        <div class="nowShowingCard">
+                            <img src="img_avatar.png" alt="Avatar" style="width:100%">
+                            <div class="container">
+                                <h4>Dumbo</h4> 
+                                <h6> SHOWING:</h6>
+                                <p>
+                                    Mon - Tues: 12pm (T12)<br>
+                                    Wed - Fri: 6pm (T18)<br>
+                                    Sat - Sun: 12pm (T12)<br>
+                                </p>
+                            </div>
+                        </div>
+
+                        <div class="nowShowingCard">
+                            <img src="img_avatar.png" alt="Avatar" style="width:100%">
+                            <div class="container">
+                                <h4>The Happy Prince</h4> 
+                                <h6> SHOWING:</h6>
+                                <p>
+                                    Wed - Fri: 12pm (T12)<br>
+                                    Sat - Sun: 9pm (T21)<br>
+                                </p>
+                            </div>
+                        </div>  
+
+
+
+
                     </P>
                 </article>
-                
-                 <article  id="linktoSynopsis">
-                       <br>
+
+                <article  id="linktoSynopsis">
+                    <br>
                     <br>
                     <h1>Synopsis</h1>
-                    <P>
-                        <br>
-                        cat
-                        <br>
-                        dog
-                        <br>
-                        horse
-                        <br>
-                        pig
-                        <br>
-                        chimp
-                        <br>
-                        greg
-                        <br>
-                        paul
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                    </P>
+                    
+                    
+                    <div class="synopsisPanel">
+                        <img src="img_avatar.png" alt="Avatar">
+                        <div class="container">
+                            <h4>Dumbo - [G]</h4> 
+                            <h6> Plot Description:</h6>
+                            <p>
+                                Crazy kids movie about some elephant that does stuff
+                            </p>
+                        </div>
+                        <div>
+                            <iframe width="560" height="315" src="https://www.youtube.com/embed/7NiYVoqBt-8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </div>
+                    </div>
+                    
+                    
+                    
+                    <div class="synopsisPanel">
+                        <img src="img_avatar.png" alt="Avatar">
+                        <div class="container">
+                            <h4>Avengers: Endgame - [MA15+]</h4> 
+                            <h6> Plot Description:</h6>
+                            <p>
+                                The avengers are at it again, avenging all sorts of things when and wherever they please
+                            </p>
+                        </div>
+                        <div>
+                           <iframe width="560" height="315" src="https://www.youtube.com/embed/TcMBFSGVi1c" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </div>
+                    </div>
+                    
+                    <div class="synopsisPanel">
+                        <img src="img_avatar.png" alt="Avatar">
+                        <div class="container">
+                            <h4>The Happy Prince - [PG13+]</h4> 
+                            <h6> Plot Description:</h6>
+                            <p>
+                                How could one prince possibly be so happy?? Find out in this period comedu
+                            </p>
+                        </div>
+                        <div>
+                            <iframe width="560" height="315" src="https://www.youtube.com/embed/4HmN9r1Fcr8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </div>
+                    </div>
+                    
+                    <div class="synopsisPanel">
+                        <img src="img_avatar.png" alt="Avatar">
+                        <div class="container">
+                            <h4>Top End Wedding - [M]</h4> 
+                            <h6> Plot Description:</h6>
+                            <p>
+                                A celebration of love, family and belonging, set at a wedding for top ends.
+                            </p>
+                        </div>
+                        <div>
+                           <iframe width="560" height="315" src="https://www.youtube.com/embed/uoDBvGF9pPU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </div>
+                    </div>
+                    
+                    
                 </article>
-                
-                 <article  id="linktoBooking">
-                       <br>
+
+                <article  id="linktoBooking">
+                    <br>
                     <br>
                     <h1>Booking</h1>
                     <P>
                         <br>
-                        cat
                         <br>
-                        dog
                         <br>
-                        horse
                         <br>
-                        pig
                         <br>
-                        chimp
                         <br>
-                        greg
                         <br>
-                        paul
+                        ******UNDER CONSTRUCTION*****
                         <br>
                         <br>
                         <br>
