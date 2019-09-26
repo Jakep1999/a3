da446e71d7d053604e4263b2fe3b13efe9744ead
Jakep1999 <37314952+Jakep1999@users.noreply.github.com>
2019-09-26 16:03:26 +1000

    all up to date and updated

--- script.js
+++ script.js
@@ -1,3 +1,149 @@
+/* Insert your javascript here */
+
+
+function showACT() {
+    
+    /*show the synopsis card */
+    document.getElementById("linktoSynopsis").style.visibility = 'visible';
+
+    
+    /* this will display this specific syponsis and hide all others */
+    document.getElementById("synopsisACT").style.display = 'block';
+    document.getElementById("synopsisANM").style.display = 'none';
+    document.getElementById("synopsisRMC").style.display = 'none';
+    document.getElementById("synopsisAHF").style.display = 'none';
+    
+    
+    /* THESE ARE FOR WHEN YOU SELECT A DIFFERENT PANEL, WIPE BOOKING */
+    /* wipe output for booking HTML*/
+    document.getElementById("movieID").innerHTML = '';
+    document.getElementById("movieDAY").innerHTML = '';
+    document.getElementById("movieHOUR").innerHTML = '';
+    
+    /* wipe the values in the form */
+    document.getElementById("movie-id").value = '';
+    document.getElementById("movie-day").value = '';
+    document.getElementById("movie-hour").value = '';
+    
+    return;
+}
+
+
+function showANM() {
+    
+    document.getElementById("linktoSynopsis").style.visibility = 'visible';
+    
+    document.getElementById("synopsisACT").style.display = 'none';
+    document.getElementById("synopsisANM").style.display = 'block';
+    document.getElementById("synopsisRMC").style.display = 'none';
+    document.getElementById("synopsisAHF").style.display = 'none';
+    
+    /* THESE ARE FOR WHEN YOU SELECT A DIFFERENT PANEL, WIPE BOOKING */
+    /* wipe output for booking HTML*/
+    document.getElementById("movieID").innerHTML = '';
+    document.getElementById("movieDAY").innerHTML = '';
+    document.getElementById("movieHOUR").innerHTML = '';
+    
+    /* wipe the values in the form */
+    document.getElementById("movie-id").value = '';
+    document.getElementById("movie-day").value = '';
+    document.getElementById("movie-hour").value = '';
+}
+
+function showRMC() {
+    
+    document.getElementById("linktoSynopsis").style.visibility = 'visible';
+
+    document.getElementById("synopsisACT").style.display = 'none';
+    document.getElementById("synopsisANM").style.display = 'none';
+    document.getElementById("synopsisRMC").style.display = 'block';
+    document.getElementById("synopsisAHF").style.display = 'none';
+    
+    /* THESE ARE FOR WHEN YOU SELECT A DIFFERENT PANEL, WIPE BOOKING */
+    /* wipe output for booking HTML*/
+    document.getElementById("movieID").innerHTML = '';
+    document.getElementById("movieDAY").innerHTML = '';
+    document.getElementById("movieHOUR").innerHTML = '';
+    
+    /* wipe the values in the form */
+    document.getElementById("movie-id").value = '';
+    document.getElementById("movie-day").value = '';
+    document.getElementById("movie-hour").value = '';
+}
+
+function showAHF() {
+    
+    document.getElementById("linktoSynopsis").style.visibility = 'visible';
+
+    document.getElementById("synopsisACT").style.display = 'none';
+    document.getElementById("synopsisANM").style.display = 'none';
+    document.getElementById("synopsisRMC").style.display = 'none';
+    document.getElementById("synopsisAHF").style.display = 'block';
+    
+     /* THESE ARE FOR WHEN YOU SELECT A DIFFERENT PANEL, WIPE BOOKING */
+    /* wipe output for booking HTML*/
+    document.getElementById("movieID").innerHTML = '';
+    document.getElementById("movieDAY").innerHTML = '';
+    document.getElementById("movieHOUR").innerHTML = '';
+    
+    /* wipe the values in the form */
+    document.getElementById("movie-id").value = '';
+    document.getElementById("movie-day").value = '';
+    document.getElementById("movie-hour").value = '';
+}
+
+function book(id, day, time) {
+    
+    var movieIDs= ["ACT", "ANM", "RMC", "AHF"];
+    var movieDAYs = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"];
+    var movieHOURs = ["T12", "T15", "T18", "T21"];
+    
+    document.getElementById("bookingsCard").style.display = 'block';
+    
+    
+    document.getElementById("movieID").innerHTML = movieIDs[id];
+    document.getElementById("movie-id").value = movieIDs[id];
+    
+    document.getElementById("movieDAY").innerHTML = movieDAYs[day];
+    document.getElementById("movie-day").value = movieDAYs[day];
+    
+    document.getElementById("movieHOUR").innerHTML = movieHOURs[time];
+    document.getElementById("movie-hour").value = movieHOURs[time];
+    
+}
+
+
+
+/* scroll changes active links */
+let mainNavLinks = document.querySelectorAll("nav ul li a");
+let mainSections = document.querySelectorAll("main section");
+let lastId;
+let cur = [];
+
+window.addEventListener("scroll", event => {
+  let fromTop = window.scrollY;
+
+  mainNavLinks.forEach(link => {
+    let section = document.querySelector(link.hash);
+
+    if (
+      section.offsetTop <= fromTop &&
+      section.offsetTop + section.offsetHeight > fromTop
+    ) {
+      link.classList.add("active");
+    } else {
+      link.classList.remove("active");
+    }
+  });
+});
+
+
+
+
+
+
+
+/* ----- JAKES CODE ---- */
 var seat_prices = {
   "STA": 19.80,
   "STP": 17.50,
@@ -10,21 +156,15 @@ var seat_prices = {
 
 window.addEventListener("load",function() { // when page has loaded
   document.getElementById("container").addEventListener("change",function(e) {
-    
+
     var tgt = e.target; // whatever was changed
-    if (tgt.tagName==="SELECT") { 
+    if (tgt.tagName==="SELECT") {
       var total = 0;
       Object.keys(seat_prices).forEach(function(key) { // get the keys from the object
         var val = document.getElementById("seats-"+key).value;
         total += val==="ticketNumber"?0: val*seat_prices[key]; // gets the amount
       })
-      document.getElementById("total").value=total.toFixed(2); // shows result with two decimals 
+      document.getElementById("total").value=total.toFixed(2); // shows result with two decimals
     }
   })
 })
-	
-
-
-
-
-
